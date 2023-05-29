cb90739d87d75991544ddb1e63d4635df89a79c4
Mateus Maschio Calderam <125042084+MateusMCalderam@users.noreply.github.com>
2023-05-29 17:22:01 -0300

    Tabela IMC

--- IMC_Calculator/src/calculadora.jsx
+++ IMC_Calculator/src/calculadora.jsx
@@ -27,7 +27,44 @@ const index = () => {
 
             <button onClick={CalcIMC}>CALCULAR</button> 
             <br />
-            <h1>{result}</h1>
+            <h1>O seu IMC é {result}</h1>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>IMC</th>
+                            <th>Classificações</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Menor que 18,5</td>
+                            <td>Abaixo do peso normal</td>
+                        </tr>
+                        <tr>
+                            <td>18,5 - 24,9</td>
+                            <td>Peso normal</td>
+                        </tr>
+                        <tr>
+                            <td>25 - 29,9</td>
+                            <td>Excesso de Peso</td>
+                        </tr>
+                        <tr>
+                            <td>30 - 34,9</td>
+                            <td>Obesidade Classe 1</td>
+                        </tr>
+                        <tr>
+                            <td>35 - 39,9</td>
+                            <td>Obesidade Classe 2</td>
+                        </tr>
+                        <tr>
+                            <td>Maior ou igual a 40</td>
+                            <td>Obesidade Classe 3</td>
+                        </tr>
+
+                    </tbody>
+                </table>
+            </div>
         </div>
     )
 
