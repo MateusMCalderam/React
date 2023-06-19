791aed3f17f0ebfcf35ba45b9df3947873e57144
Mateus Maschio Calderam <125042084+MateusMCalderam@users.noreply.github.com>
2023-06-19 08:25:40 -0300

    Update

--- my-components/src/main.jsx
+++ my-components/src/main.jsx
@@ -5,6 +5,7 @@ import {CompA, CompB as B} from './componentes/DoisComponentes';
 import MultiElementos from './componentes/MultiElementos';
 import ComponenteClasse from './componentes/ComponenteClasse';
 import ComponenteComFuncao from './componentes/ComponenteComFuncao';
+import Provider from './componentes/comtext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -15,7 +16,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     </div>
     <MultiElementos/> 
     <ComponenteClasse valor="Esse é o valor passado dentro do props em um Componente Classe"/>
-
     <ComponenteComFuncao/> 
+
+    <Provider value={color: 'blue'}>
+      {children}
+    </Provider>
   </React.StrictMode>
 )
